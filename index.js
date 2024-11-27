58638de2375c6e650cc631e696fd0dd85d517313
Nexorisolutions <nexorisolutions@gmail.com>
2024-11-27 17:38:18 +0300

    add Website

---
+++ index.js
@@ -0,0 +1,17 @@
+// Open and close chatbot window
+
+// Select the navbar
+
+// JavaScript for the hero slider
+
+// Submit form via WhatsApp
+
+// FAQ Toggle Functionality
+
+// Contant-Us Toggle Functionality
+
+// testimonial Toggle Functionality
+
+// carousel-track Toggle Functionality
+
+
